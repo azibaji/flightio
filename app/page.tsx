bd7e52827a2a7fa86687b6028fb2deefe8548754
a.saremian <a.saremian@hasin.ir>
2024-07-06 16:00:28 +0330

    fxi naming

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Filter from "@/app/components/Products/FilterProducts/FilterProducts";
-import ProductsList from "@/app/components/Products/ProductsList/productsList";
+import ProductsList from "@/app/components/Products/ProductsList";
 
 export default function Home() {
   return (
